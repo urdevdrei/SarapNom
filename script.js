77e935775948287d2ee4b690daf8ba22701c6318
urdevdrei <santosandrei079@gmail.com>
2026-04-21 22:16:59 +0800

    remove scrpt.js and product

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-// Mobile menu toggle
+// mobile menu 
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
+
 
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
 
-// Close menu on link click
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', () => {
-        navMenu.classList.remove('active');
-    });
-});
